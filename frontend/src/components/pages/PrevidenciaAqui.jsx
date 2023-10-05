454da7c5859659e154fb9886f77540836ea03f4d
Eric Ribeiro <eric.r@aluno.ifsp.edu.br>
2023-10-05 16:05:00 -0300

    fix: Arrumado erros de digitação em imports

--- frontend/src/components/pages/PrevidenciaAqui.jsx
+++ frontend/src/components/pages/PrevidenciaAqui.jsx
@@ -1,6 +1,11 @@
-import Header from '../layout/Header';
-import Footer from '../layout/Footer';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
 
+const PrevidenciaAqui = () => {
+    return (
+        <h1>Fazendo...</h1>
+    );
+}
 
 
 export default PrevidenciaAqui;
