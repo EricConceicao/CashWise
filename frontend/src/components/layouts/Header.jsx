c7ae481930ed2792c84532feb58bada2edc2ad49
Eric <eric.r@aluno.ifsp.edu.br>
2023-10-07 00:54:31 -0300

    style: Adicionado uma animaçãozinha de hover personalizada para os links do header,

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -3,20 +3,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
 
+import './Header.css';
+
 function Header() {
     return (
-        <Navbar expand="md" className="z-1 position-sticky top-0 bg-primary">
+        <Navbar expand="md" className="position-sticky z-0 top-0 p-0 bg-primary">
             <Container>
                 <Navbar.Brand href='#foi'>
-                    <img className="w-50 rounded-pill" 
+                    <img className="rounded-pill" 
                     src="/img/logo-cashwise.jpg" 
                     alt="Logo marca do CashWise"
-                    title="CashWise" />
+                    title="CashWise. Educação Financeira"
+                    width="42%" />
                 </Navbar.Brand>
 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="gap-2 me-auto" id="header-nav">
+                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                    <Nav className="fw-bold gap-2" id="header-nav">
                         <NavLink className="nav-link" to="/home">Home</NavLink>
                         <NavLink className="nav-link" to="/boletim">Boletim Informativo</NavLink>
                         <NavLink className="nav-link" to="/previdencia">Previdência Aqui</NavLink>
