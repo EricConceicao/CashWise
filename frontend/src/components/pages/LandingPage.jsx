546514aa500b602b7bfcbbd6a35a682a19cec96a
Eric Ribeiro <eric.r@aluno.ifsp.edu.br>
2023-10-16 17:46:20 -0300

    feat: Colocando textos na landing page

--- frontend/src/components/pages/LandingPage.jsx
+++ frontend/src/components/pages/LandingPage.jsx
@@ -1,3 +1,5 @@
+import {NavLink} from 'react-router-dom';
+
 // Importações de componentes próprios //
 import Header from '../layouts/Header';
 import Article from '../layouts/Article';
@@ -7,10 +9,15 @@ import Footer from '../layouts/Footer';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
+
+/* Texto pra mim mesmo usar em casa depois.
+Previdência Social
+Entenda como o sistema de previdência social funciona e descubra maneiras de otimizar seus benefícios no futuro.
+*/
 
 import './LandingPage.css';
 
@@ -26,7 +33,7 @@ const LandingPage = () => {
                         title="CashWise. Educação Financeira"
                         width="42%" />
                     </Navbar.Brand>
-
+        
                     <Button variant="secondary" size="lg">Entrar</Button>
                 </Container>
             </header>
@@ -44,20 +51,26 @@ const LandingPage = () => {
             
             <main>
                 <Container fluid className="bg-secondary pt-5">
+                    <p className='text-light fs-3 container text-center my-4 py-2 border-bottom'>
+                    <span className="lead text-primary fs-2">Bem-vindo ao nosso Sistema de Educação Financeira!</span> Aqui você encontrará ferramentas e recursos para ajudar jovens e 
+                    adultos a alcançarem um futuro financeiro estável e confortável. 
+                    Nós acreditamos que um <strong>planejamento sólido é a chave</strong> para uma vida 
+                    financeira tranquila. Comece sua jornada hoje mesmo!
+                    </p>
                     <Row>
                         <Col md="6">
-                            <Article title="Controle seus gastos" img="/img/menino-no-puf.svg" alt="Desenho de uma pessoa sentada em um puf com um notebook">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima doloribus aliquam tempore vel itaque minus laudantium, beatae ipsam dolore sequi illum, possimus dolor ab.
+                            <Article title="Controle orçamentário" img="/img/cofrinho.svg" alt="Desenho de uma pessoa sentada em um puf com um notebook">
+                            Aprenda a administrar suas finanças diárias. Defina metas, categorize seus gastos e acompanhe seu orçamento de forma eficaz.
                             </Article>
                         </Col>
                         <Col md="6">     
-                            <Article title="Saiba o que fazer com seu dinheiro" img="/img/cofrinho.svg" alt="Desenho de uma pessoa colocando moedas em um porquinho porta moedas.">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima doloribus aliquam tempore vel itaque minus laudantium, beatae ipsam dolore sequi illum, possimus dolor ab.
+                            <Article title="Aprenda sobre investimentos" img="/img/menino-no-puf.svg" alt="Desenho de uma pessoa colocando moedas em um porquinho porta moedas.">
+                            Visualize diferentes cenários de investimentos e renda passiva. Aprenda a tomar decisões acertadas sobre como fazer seu dinheiro crescer ao longo do tempo.
                             </Article>
                         </Col>
                         <Col md="12" lg="12">
-                            <Article title="Planeje o seu futuro" img="/img/homi-das-plantas.svg" alt="Desenho de um homem regando plantas dando 'moedas' como frutos.">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam minima doloribus aliquam tempore vel itaque minus laudantium, beatae ipsam dolore sequi illum, possimus dolor ab.
+                            <Article title="Tenha controle com sua agenda" img="/img/homi-das-plantas.svg" alt="Desenho de um homem regando plantas dando 'moedas' como frutos.">
+                            Mantenha-se organizado com nossa agenda financeira. Marque datas importantes, prazos de pagamento e lembretes para garantir que suas finanças estejam sempre em dia.    
                             </Article>
                         </Col>
                     </Row>
@@ -66,8 +79,6 @@ const LandingPage = () => {
                         <Button className="w-50 mx-auto p-2 my-3 fs-4 fw-bold" size="large" variant="primary">Acesse já!</Button>
                     </div>
                 </Container>
-
-                
             </main>
 
             <Footer anchor="hero" />
