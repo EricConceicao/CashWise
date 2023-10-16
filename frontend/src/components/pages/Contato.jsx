46492223be0f3bed8604692a07fbb8e0c4084d4c
selmabras <selma.b@aluno.ifsp.edu.br>
2023-10-16 17:21:25 -0300

    formulário para contato com validação visual

--- frontend/src/components/pages/Contato.jsx
+++ frontend/src/components/pages/Contato.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 // Importações dos nossos componentes
 import Content from "../layouts/Content"
 import Header from "../layouts/Header"
@@ -13,23 +13,34 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 
-//Importações Validação
-
-import { useState } from 'react';
-import Feedback from 'react-bootstrap/Feedback';
-
 function Contato() {
 
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
   const [validated, setValidated] = useState(false);
+  const [nome, setNome] = useState('');
+  const [assunto, setAssunto] = useState('');
+  const [mensagem, setMensagem] = useState('');
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  const handleEmailChange = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsValidEmail(emailRegex.test(inputValue));
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setValidated(true);
+    if (isValidEmail && nome && assunto && mensagem) {
+      console.log('E-mail válido:', email);
+      console.log('Nome:', nome);
+      console.log('Assunto:', assunto);
+      console.log('Mensagem:', mensagem);
+
+    } else {
+      console.log('Por favor, preencha todos os campos corretamente.');
+    }
   };
 
   const containerStyle = {
@@ -55,58 +66,90 @@ function Contato() {
   };
 
   return (
-
     <div style={containerStyle}>
       <Header />
       <Container className="my-4">
-        <Row className="justify-content-center align-items-center">
-          <Col md={2}>
-            <img src="../img/correio.jpg" alt="Logo" style={{ height: '150px', animation: 'swing 2s linear infinite', borderRadius: "50%" }} />
-          </Col>
-          <Col className='justify-content-center' md={6} >
-            <h1>Contato</h1>
-            <h4>Envie um Feedback</h4>
-          </Col>
-        </Row>
-        <Row className="justify-content-center align-items-center">
-          <Col md={6} >
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Row className="justify-content-center align-items-center">
+            <Col md={2}>
+              <img src="../img/correio.jpg" alt="Logo" style={{ height: '150px', animation: 'swing 2s linear infinite', borderRadius: "50%" }} />
+            </Col>
+            <Col className='justify-content-center' md={6} >
+              <h1>Contato</h1>
+              <h4>Envie um Feedback</h4>
+            </Col>
+          </Row>
+          <Row className="justify-content-center align-items-center">
+            <Col md={6} >
+
               <Form.Group controlId="validationCustom01" style={formGroupStyle}>
                 <Form.Label>Nome</Form.Label>
-                <Form.Control required
+                <Form.Control
+                  required
                   type="text"
                   placeholder="Nome"
-                  defaultValue="Digite seu nome" style={inputStyle} />
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  style={inputStyle}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Por favor, insira seu nome.
+                </Form.Control.Feedback>
               </Form.Group>
-              <Form.Group controlId="validationCustomUsername" style={formGroupStyle}>
-                <Form.Label>Email</Form.Label>
 
-                <InputGroup hasValidation>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Email</Form.Label>
                   <Form.Control
-                    type="email" obrigatório is isValid />
-                    <Form.Control.Feedback type="inválido">
-                      Digite seu Email
-                    </Form.Control.Feedback>
-                   
-                </InputGroup>
+                    type="email"
+                    placeholder="nome@example.com"
+                    value={email}
+                    onChange={handleEmailChange}
+                    isInvalid={!isValidEmail}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Por favor, insira um e-mail válido.
+                  </Form.Control.Feedback>
+                </Form.Group>
 
-              </Form.Group>
-              <Form.Group controlId="formAssunto" style={formGroupStyle}>
-                <Form.Label>Assunto</Form.Label>
-                <Form.Control type="text" placeholder="" style={inputStyle} />
-              </Form.Group>
-            </Form>
-            <Form.Group controlId="formMensagem" style={formGroupStyle}>
-              <Form.Label>Mensagem</Form.Label>
-              <Form.Control as="textarea" rows={4} placeholder="Digite a sua mensagem" style={inputStyle} />
-            </Form.Group>
-            <Button variant="primary" type="submit" style={buttonStyle}>
-              Enviar
-            </Button>
-          </Col>
-        </Row>
+                <Form.Group controlId="formAssunto" style={formGroupStyle}>
+                  <Form.Label>Assunto</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder=""
+                    value={assunto}
+                    onChange={(e) => setAssunto(e.target.value)}
+                    style={inputStyle}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Por favor, insira o assunto.
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group controlId="formMensagem" style={formGroupStyle}>
+                  <Form.Label>Mensagem</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={4}
+                    placeholder="Digite a sua mensagem"
+                    value={mensagem}
+                    onChange={(e) => setMensagem(e.target.value)}
+                    style={inputStyle}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Por favor, insira a mensagem.
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Button variant="primary" type="submit" style={buttonStyle}>
+                  Enviar
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Form>
       </Container>
       <Footer />
     </div>
