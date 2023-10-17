ae685b2449c5f780339d1da7f5f3606b4314c6f1
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-10-17 16:01:57 -0300

    ajuste Sobre

--- frontend/src/components/pages/Sobre.jsx
+++ frontend/src/components/pages/Sobre.jsx
@@ -6,33 +6,49 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
+const containerStyle = {
+    backgroundImage: 'linear-gradient(to bottom, #213740, #5BD992, #AEF2C6)',
+    color: 'white',
+};
+const titleStyle = {
+    padding: '10px',
+    margin: '10px'
+}
 
-import './Sobre.css';
+const cardStyle = {
+    min: '12rem',
+    max: '20rem',
+    margin: '1rem',
+};
 
 
 const Sobre = () => {
     return (
         <>
             <Header />
-            <div className="container-fluid" id='main'>
-                <Content>
-                    <div className="jumbotron text-center" id='topo'>
-                        <h1 >Sobre o CashWise</h1>
-                    </div>
-                    <Container fluid className='row'>
-                        <Col xs={9}><p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, reiciendis! Iusto perspiciatis commodi animi. Quo dicta aliquam magnam iure, placeat recusandae laudantium odio nostrum rerum similique reiciendis consequatur, ipsum doloremque?</p></Col>
+            <div className="container-fluid" id='main' style={containerStyle}>
+                <Content >
+                    <Container fluid className='row' id='main'>
+                        <Col xs={12}>
+                            <div className="jumbotron text-center" id='topo' >
+                                <h1 style={titleStyle}>Sobre o CashWise</h1>
+                            </div>
+                        </Col>
+                        <Col xs={9}>
+                            <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, reiciendis! Iusto perspiciatis commodi animi. Quo dicta aliquam magnam iure, placeat recusandae laudantium odio nostrum rerum similique reiciendis consequatur, ipsum doloremque?</p>
+                        </Col>
                         <Col>
-                                <img src="/img/fintechs-transparente.webp" alt="" className='figure-img img-fluid rounded' />
+                            <img src="/img/fintechs-transparente.webp" alt="" className='figure-img img-fluid rounded' />
                         </Col>
                     </Container>
 
-                    <Container fluid id='cardsAlunos' className='row mb-5'>
+                    <Container fluid id='cardsAlunos' className='row mb-2 justify-content-around'>
 
-                        <div className="col-sm-2 col-md-4" >
-                            <Card className="card">
+                        <div className="col-lg-2" >
+                            <Card className="card" style={cardStyle}>
                                 <Card.Img variant="top" src="/img/aguia.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title 1</Card.Title>
+                                    <Card.Title>Eric</Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the
                                         bulk of the card's content.
@@ -41,11 +57,11 @@ const Sobre = () => {
                             </Card>
                         </div>
 
-                        <div className="col-sm-2 col-md-4">
-                            <Card>
+                        <div className="col-lg-2">
+                            <Card className="card" style={cardStyle}>
                                 <Card.Img variant="top" src="/img/aguia.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title 2</Card.Title>
+                                    <Card.Title>Maria Selma</Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the
                                         bulk of the card's content.
@@ -54,11 +70,11 @@ const Sobre = () => {
                             </Card>
                         </div>
 
-                        <div className="col-sm-2 col-md-4">
-                            <Card>
+                        <div className="col-lg-2">
+                            <Card className="card" style={cardStyle}>
                                 <Card.Img variant="top" src="/img/aguia.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title 3</Card.Title>
+                                    <Card.Title>Renato</Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the
                                         bulk of the card's content.
@@ -67,11 +83,11 @@ const Sobre = () => {
                             </Card>
                         </div>
 
-                        <div className="col-sm-2 col-md-4">
-                            <Card>
+                        <div className="col-lg-2">
+                            <Card className="card" style={cardStyle}>
                                 <Card.Img variant="top" src="/img/aguia.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title 4</Card.Title>
+                                    <Card.Title>Tatiana</Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the
                                         bulk of the card's content.
@@ -80,11 +96,11 @@ const Sobre = () => {
                             </Card>
                         </div>
 
-                        <div className="col-sm-2 col-md-4">
-                            <Card>
+                        <div className="col-lg-2">
+                            <Card className="card" style={cardStyle}>
                                 <Card.Img variant="top" src="/img/aguia.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card Title 5</Card.Title>
+                                    <Card.Title>Valdimir</Card.Title>
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the
                                         bulk of the card's content.
