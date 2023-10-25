10b7ca217d22c7e56c85f585e7f91b5b640b6ad1
Eric <eric.r@aluno.ifsp.edu.br>
2023-10-25 01:21:03 -0300

    feat: adicionado o dotenv; Movido a pasta do prisma para o lugar certo; Adicionado hash com salt para as senhas; Adicionado login; Adicionado feedback caso os dados venham incompletos e etc.

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -3,6 +3,7 @@ import { signup, login } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.post('/', signup)
+router.post('/signup', signup);
+router.post('/login', login);
 
 export default router
--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,20 @@
 // Importações de bibliotecas e pacotes
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+
+// Importando as variáveis de ambiente do .env
+import { config } from 'dotenv';
+config();
 
 const api = express();
 
 // Definindo middlewares
 api.use(cors());
-api.use(bodyParser.json());
+api.use(express.json());
+
 
 // Rotas //
+
 // Importações
 import authRouter from './routes/auth.routes.js';
 
@@ -21,8 +26,9 @@ api.use('/', (req, res) => {
 	res.status(200).json({ success: true, message: 'Bem vindo ao lugar nenhum da nossa API :3'});
 });
 
+
 // Configurações da api
-const PORT = 3000;
-api.listen(PORT, (req, res) => {
-	console.log(`Api online na porta ${PORT}`);
+const port = process.env.PORT || 3000;
+api.listen(port, (req, res) => {
+	console.log(`Api online na porta ${port}`);
 });
