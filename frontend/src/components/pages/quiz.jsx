c99d4cf78d75a19405adb1b5d3d2003826eff1b1
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-10-26 14:45:27 -0300

    trabalhando componente quiz

---
+++ frontend/src/components/pages/quiz.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap';
+import { useEffect, useState } from "react"
+
+
+const Quiz = () => {
+
+    return(
+        <>
+
+        </>
+
+    );
+};
+
+export default Quiz;
