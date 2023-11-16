ee8f0b0a9990025496b8601a4d450a553e17f7e2
Eric <eric.r@aluno.ifsp.edu.br>
2023-11-16 13:52:11 -0300

    feat!: Ajustes finais na loja de ícones.

--- frontend/src/components/utils/IconShop.jsx
+++ frontend/src/components/utils/IconShop.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // Imports do BS
 import Modal from 'react-bootstrap/Modal';
@@ -10,21 +10,24 @@ import Row from 'react-bootstrap/Row';
 // Store do usuário
 import useUserStore from '../store/UserStore';
 
-import { PiCoinsBold as Coin } from 'react-icons/pi';
 
 function IconShop() {
+
+    // Hooks //
     const [showShop, setShowShop] = useState(false);
     const [data, setData] = useState([]);
-    
-    const photo = useUserStore(state => state.photo);
     const [pfp, setPfp] = useState(null);
-
-    const changePhoto = useUserStore(state => state.changePhoto);
+    const [coins, setCoins] = useState(null);
+    const [feedback, setFeedback] = useState('');
+    // Variáveis do Store //
+    const wiseCoins = useUserStore(state => state.wiseCoins);
+    const photo = useUserStore(state => state.photo);
     const token = useUserStore(state => state.userToken);
+    const changePhoto = useUserStore(state => state.changePhoto);
+    const refreshCoins = useUserStore(state => state.refreshCoins);
 
-    useEffect(() =>{
-        setPfp(photo);
-    },[photo]);
+    useEffect(() =>{setPfp(photo)}, [photo]);
+    useEffect(() =>{setCoins(wiseCoins)}, [wiseCoins]);
 
     // Função para requerir os dados com os ícones disponiveis no banco //
     useEffect(() => {
@@ -41,21 +44,21 @@ function IconShop() {
 
                 if (iconData.success) {
                     setData(iconData.iconsData);
-                    console.log(data);
                     return
                 } else {
-                    alert(iconData.message)
+                    setFeedback(iconData.message);
                     return
                 }
             } else {
-                alert('Erro de servidor interno');
+                const { message } = await res.json()
+                setFeedback(message);
                 return
             }
         }
 
         // Espera o token receber os dados do loader para então chamar esta função
         if (token) getIcons();
-    }, [token]);
+    }, [token, pfp]);
 
     async function handleShop(photo) {
         const res = await fetch('http://localhost:3000/shop', {
@@ -70,20 +73,41 @@ function IconShop() {
 
         if (res.ok) {
             const data = await res.json();
-
             if (data.success) {
+                // Atualiza o store
+                if (typeof data.newBalance !== undefined) {refreshCoins(data.newBalance)}
                 changePhoto(photo);
+                // Atualiza os valores na página
                 setPfp(photo);
-
+                setCoins(newBalance);
+            
                 return 
             } else {
-                alert(data.message)
+                alert(data.message);
+
                 return
             }
         } else {
-            alert('Erro de servidor interno');
+            const { message } = await res.json()
+            setFeedback(message);
             return
         }
+    }
+
+    function checkObtainedIcons(icon) {
+        const result = data?.userIcons.find(user => user.iconId === icon.id)
+
+        if (result?.obtained) {
+            
+            return <span>Selecionar ícone</span>    
+        } else {
+            
+            return <> <span className="visually-hidden">Preço:</span> {icon.price} <img src="img/wisecoin.webp" width="20rem" /> </>
+        }       
+    }
+    function checkObtainedIcon(icon) {
+         const result = data?.userIcons.find(user => user.iconId === icon.id);
+         return !result.obtained
     }
 
 
@@ -108,28 +132,21 @@ function IconShop() {
             <Modal show={showShop} onHide={() => setShowShop(false)} centered>
                 <Modal.Header className='bg-warning' closeButton><span className="display-6">Loja de icones</span></Modal.Header >
                 <Modal.Body>
-                    <p>Selecione um ícone para comprar e usar no seu perfil!</p>
+                    <h1 className="h5">Selecione um ícone para comprar e usar no seu perfil!</h1>
+                    <p className="text-center bg-secondary text-white p-1 rounded-pill">Seu saldo: {coins} <img src="img/wisecoin.webp" width="24rem" /></p>
                     <Row>
                         {
                             data.icons && data.icons.map(icon => (
                             <Col sm="12" md="6" key={icon.id}>
                                 <Card style={cardItem}>
-                                    <Card.Header className='fw-medium'>Foto de perfil padrão</Card.Header>
+                                    <Card.Header className='fw-medium'>{icon.name}</Card.Header>
                                     <Card.Img style={cardImg} className='mb-2' src={icon.src} />
 
-                                    <Button disabled={pfp === icon.src} onClick={() => handleShop(icon.src)}>
+                                    <Button disabled={pfp === icon.src || wiseCoins < icon.price && checkObtainedIcon(icon)} onClick={() => handleShop(icon.src)}>
                                     { pfp === icon.src ? (
                                         "Selecionado" 
                                     ) : ( 
-                                        data.userIcons.map(userIcons => (
-                                            userIcons.iconId === icon.id && userIcons.obtained === true ? (
-                                            <span>Selecionar ícone</span>
-                                            
-                                        ) : (
-                                            <>
-                                                <span className="visually-hidden">Preço:</span> {icon.price} <img src="img/wisecoin.webp" width="24rem" />
-                                            </>
-                                        )))
+                                        checkObtainedIcons(icon)
                                     )}
                                     </Button>
                                 </Card>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Router>
 )
