77585885aeaaa13d53d9d5d30b7331d953b6b3ac
Eric Ribeiro <eric.r@aluno.ifsp.edu.br>
2023-11-16 18:02:31 -0300

    feat: Terminado a stilização dá loja de ícones

--- frontend/src/components/pages/Investimentos.jsx
+++ frontend/src/components/pages/Investimentos.jsx
@@ -8,110 +8,119 @@ import { VscError as Nocheck } from 'react-icons/vsc';
 
 const Investimentos = () => {
 
-    const divStyle = {
-        width: '100%',
-        backgroundImage: 'url("img/moldura.png")',
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        clear: 'both', textAlign: "left",
+	const divStyle = {
+		width: '100%',
+		backgroundImage: 'url("img/moldura.png")',
+		backgroundSize: 'contain',
+		backgroundPosition: 'center',
+		clear: 'both', textAlign: "left",
 
-    };
+	};
 
-    return (
-        <>
-            <Header />
-            <div style={divStyle}>
-                <div className="bg-secondary text-white text-center py-5">
-                    <h1>Principais Tipos de Investimentos</h1>
-                    <h2>Os Diferentes Tipos de Renda Fixa</h2>
-                </div>
+	return (
+		<>
+			<Header />
+			<div style={divStyle}>
+				<div className="bg-secondary text-white text-center py-5">
+					<h1>Principais Tipos de Investimentos</h1>
+					<h2>Os Diferentes Tipos de Renda Fixa</h2>
+				</div>
 
-                <Container className="mt-5 mx-auto">
-                    <Row>
-                        <Col lg={4} md={6} className="mb-4">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title className="text-center">Certificados de Depósito Bancário (CDB):</Card.Title>
-                                    <Card.Text style={{ textAlign: "justify" }}>
-                                        <Card.Img className='w-100 ' src="img/titulos.png" alt="Imagem do artigo investimentos" />
+				<Container className="mt-5 mx-auto">
+					<Row>
+						<Col lg={4} md={6} className="mb-4">
+							<Card className='h-100'>
+								<Card.Header>
+									<Card.Title className="text-center">Certificados de Depósito Bancário</Card.Title>
+									<Card.Subtitle className='text-center'>(CDB)</Card.Subtitle>
+								</Card.Header>
 
-                                        <p><Check size={20} className='text-primary' />Vantagens:</p>
-                                        <ul>
+								<Card.Body>
+									<Card.Text style={{ textAlign: "justify" }}>
+										<Card.Img style={{ minHeight: "20rem", maxHeight: "20rem" }} className='w-100' src="img/titulos.png" alt="Imagem do artigo investimentos" />
 
-                                            <li>Segurança: Garantido pelo Fundo Garantidor de Créditos (FGC) até um determinado valor.</li>
+										<p><Check size={20} className='text-primary' />Vantagens:</p>
+										<ul>
 
-                                            <li>Variedade de Prazos: CDBs podem ter diferentes prazos, permitindo escolher uma opção que se alinhe com seus objetivos financeiros.</li>
+											<li>Segurança: Garantido pelo Fundo Garantidor de Créditos (FGC) até um determinado valor.</li>
 
-                                            <li>Rentabilidade: Oferece taxas de juros mais atrativas do que a poupança, dependendo do prazo e do banco emissor.</li>
-                                        </ul>
+											<li>Variedade de Prazos: CDBs podem ter diferentes prazos, permitindo escolher uma opção que se alinhe com seus objetivos financeiros.</li>
 
-                                        <p><Nocheck size={20} className='text-danger' />Desvantagens:</p>
-                                        <ul>
-                                            <li>Imposto de Renda: Sujeito à tributação de Imposto de Renda, com alíquotas decrescentes conforme o tempo de investimento.
-                                            </li>
-                                        </ul>
-                                    </Card.Text>
-                                </Card.Body>
+											<li>Rentabilidade: Oferece taxas de juros mais atrativas do que a poupança, dependendo do prazo e do banco emissor.</li>
+										</ul>
 
-                            </Card>
-                        </Col>
+										<p><Nocheck size={20} className='text-danger' />Desvantagens:</p>
+										<ul>
+											<li>Imposto de Renda: Sujeito à tributação de Imposto de Renda, com alíquotas decrescentes conforme o tempo de investimento.
+											</li>
+										</ul>
+									</Card.Text>
+								</Card.Body>
 
-                        <Col lg={4} md={6} className="mb-4">
-                            <Card>
+							</Card>
+						</Col>
 
-                                <Card.Body>
-                                    <Card.Title className="text-center">Tesouro Direto </Card.Title>
-                                    <h6 className='text-center'>(Títulos Públicos):</h6>
-                                    <Card.Text style={{ textAlign: "justify" }}>
-                                        <Card.Img className='w-100 ' src="img/tesouro.png" alt="Imagem do artigo investimentos" />
-                                        <p><Check size={20} className='text-primary' />Vantagens:</p>
-                                        <ul>
-                                            <li>Segurança: Investimento em títulos públicos, considerados de baixo risco.</li>
+						<Col lg={4} md={6} className="mb-4">
+							<Card className='h-100'>
+								<Card.Header>
+									<Card.Title className="text-center">Tesouro Direto </Card.Title>
+									<Card.Subtitle className='text-center'>Títulos Públicos</Card.Subtitle>
+								</Card.Header>
 
-                                            <li>Acessibilidade: Investidores individuais podem comprar títulos do Tesouro Direto com baixo valor inicial.</li>
+								<Card.Body>
+									<Card.Text style={{ textAlign: "justify" }}>
+										<Card.Img style={{ minHeight: "20rem", maxHeight: "20rem" }} className='w-100 ' src="img/tesouro.png" alt="Imagem do artigo investimentos" />
+										<p><Check size={20} className='text-primary' />Vantagens:</p>
+										<ul>
+											<li>Segurança: Investimento em títulos públicos, considerados de baixo risco.</li>
 
-                                            <li>Diversidade: Diferentes tipos de títulos, como Tesouro Selic, Tesouro IPCA+ e Tesouro Prefixado, oferecendo opções para diferentes perfis de investidores.</li>
-                                        </ul>
+											<li>Acessibilidade: Investidores individuais podem comprar títulos do Tesouro Direto com baixo valor inicial.</li>
 
-                                        <span><Nocheck size={20} className='text-danger' />Desvantagens:</span>
-                                        <ul>
-                                            <li>Flutuação de Preços: Os preços dos títulos podem variar no mercado secundário, impactando o valor dos investimentos antes do vencimento.</li>
-                                            <li>Tributação: Incide Imposto de Renda sobre os rendimentos, seguindo uma tabela regressiva.</li>
-                                        </ul>
+											<li>Diversidade: Diferentes tipos de títulos, como Tesouro Selic, Tesouro IPCA+ e Tesouro Prefixado, oferecendo opções para diferentes perfis de investidores.</li>
+										</ul>
 
-                                    </Card.Text>
-                                </Card.Body>
+										<span><Nocheck size={20} className='text-danger' />Desvantagens:</span>
+										<ul>
+											<li>Flutuação de Preços: Os preços dos títulos podem variar no mercado secundário, impactando o valor dos investimentos antes do vencimento.</li>
+											<li>Tributação: Incide Imposto de Renda sobre os rendimentos, seguindo uma tabela regressiva.</li>
+										</ul>
+									</Card.Text>
+								</Card.Body>
 
-                            </Card>
-                        </Col>
+							</Card>
+						</Col>
 
-                        <Col lg={4} md={6} className="mb-4">
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title className="text-center">Letra de Crédito Imobiliário (LCI/LCA)</Card.Title>
+						<Col lg={4} md={6} className="mb-4">
+							<Card className='h-100'>
+								<Card.Header>
+									<Card.Title className="text-center">Letra de Crédito Imobiliário</Card.Title>
+									<Card.Subtitle className='text-center'>(LCI/LCA)</Card.Subtitle>
+								</Card.Header>
 
-                                    <Card.Text style={{ textAlign: "justify" }} >
-                                        <Card.Img className='w-100 ' src="img/investimentos.png" alt="Imagem do artigo investimentos" />
-                                        <p><Check size={20} className='text-primary' />Vantagens:</p>
-                                        <p>Isenção de Imposto de Renda: Rendimentos são isentos de Imposto de Renda para pessoas físicas.</p>
-                                        <p>Segurança: Garantidos pelo Fundo Garantidor de Créditos (FGC) em caso de falência da instituição financeira emissora.</p>
-                                        <p>Específicos: LCI está atrelada a financiamentos imobiliários; LCA está relacionada ao agronegócio.</p>
+								<Card.Body className='h-100'>
+									<Card.Img style={{ minHeight: "20rem", maxHeight: "20rem" }} className='w-100' src="img/investimentos.png" alt="Imagem do artigo investimentos" />
 
-                                        <p><Nocheck size={20} className='text-danger' />Desvantagens:</p>
-                                        <p>Liquidez: Alguns títulos têm prazos mínimos de carência, limitando a disponibilidade do dinheiro antes do vencimento.</p>
-                                        <p>Rendimento Menor: Em geral, oferecem taxas de juros um pouco menores em comparação com CDBs de bancos médios e grandes.</p>
-                                    </Card.Text>
-                                </Card.Body>
+									<Card.Text style={{ textAlign: "justify" }} >
+										<p><Check size={20} className='text-primary' />Vantagens:</p>
+										<p>Isenção de Imposto de Renda: Rendimentos são isentos de Imposto de Renda para pessoas físicas.</p>
+										<p>Segurança: Garantidos pelo Fundo Garantidor de Créditos (FGC) em caso de falência da instituição financeira emissora.</p>
+										<p>Específicos: LCI está atrelada a financiamentos imobiliários; LCA está relacionada ao agronegócio.</p>
 
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-                <Footer />
+										<p><Nocheck size={20} className='text-danger' />Desvantagens:</p>
+										<p>Liquidez: Alguns títulos têm prazos mínimos de carência, limitando a disponibilidade do dinheiro antes do vencimento.</p>
+										<p>Rendimento Menor: Em geral, oferecem taxas de juros um pouco menores em comparação com CDBs de bancos médios e grandes.</p>
+									</Card.Text>
+								</Card.Body>
 
-            </div>
-        </>
-    );
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+				<Footer />
+
+			</div>
+		</>
+	);
 }
 
 export default Investimentos;
