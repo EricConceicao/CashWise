1f8bd15d1568be93a570bb0ef6604100a12d0666
selmabras <selma.b@aluno.ifsp.edu.br>
2023-11-17 17:49:09 -0300

    configurações artigos

--- frontend/src/components/pages/Dicas.jsx
+++ frontend/src/components/pages/Dicas.jsx
@@ -1,12 +1,13 @@
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import { FaMoneyBillWheat as Dica } from "react-icons/fa6";
 
 const Dicas = () => {
 
     const divStyle = {
         width: '100%',
-        backgroundImage: 'url("img/papeldec.png")',
+        backgroundImage: 'url("img/moldura.png")',
         backgroundSize: 'contain',
         backgroundPosition: 'center',
 
@@ -23,38 +24,58 @@ const Dicas = () => {
                 </header>
                 <div className="container p-5 bg-light text-justfy">
 
-                    <article>
+                    <Row className="align-items-left p-2"><h2 className='fw-bold text-center p-4'>Veja como organizar suas finanças:</h2>
+                        <Col md={12} className="mb-4">
+                            <Card>
+                                <Card.Body>
+                                    <Card.Img id='card-img' src="img/dicasorg.png" alt="Imagem do artigo dicas" style={{ width: '95%', height: 'auto', float: 'left', marginRight: '10px ' }} />
+                                    <Card.Text style={{ textAlign: "justify" }}>A economia financeira refere-se à gestão eficiente dos recursos monetários disponíveis, sendo um conjunto de práticas e estratégias que visa promover a estabilidade econômica pessoal, garantindo o equilíbrio entre o presente e o futuro financeiro.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
 
-                        <Row className="align-items-left p-2"><h2 className='fw-bold text-center p-4'>Veja como organizar suas finanças:</h2>
-                            <Col sm="6">
-                                <img src="img/dicasorg.png" alt="Imagem do artigo dicas" style={{ width: '95%', height: 'auto', float: 'left', marginRight: '10px ' }} />
-                            </Col>
+                        <Col lg={6} md={6} className="mb-4">
+                            <Card>
+                                <Card.Body>                                   
 
-                            <Col sm="6" className='px-2'>
-                                
-                                <p className='fs-4 p-3'>A economia financeira refere-se à gestão eficiente dos recursos monetários disponíveis, sendo um conjunto de práticas e estratégias que visa promover a estabilidade econômica pessoal, garantindo o equilíbrio entre o presente e o futuro financeiro.</p>
-                            </Col>
-                        </Row>
+                                    <Card.Text>
+                                        <ul> < Dica size={150} className='fs-4' />
+                                            <li>Planeje seu orçamento mensal para garantir que suas despesas não ultrapassem sua renda;</li>
+                                            <li>Estabeleça metas financeiras realistas e crie um plano para alcançá-las gradualmente;</li>
+                                            <li>Mantenha um registro detalhado de todas as suas despesas para identificar áreas de oportunidade de economia;</li>
+                                            <li>Priorize o pagamento de dívidas de juros elevados para reduzir custos a longo prazo;</li>
+                                            <li>Crie uma reserva de emergência equivalente a pelo menos três meses de despesas para lidar com imprevistos;</li>
+                                            <li>Considere investir parte de sua renda para construir um futuro financeiramente seguro;</li>
+                                            <li>Evite compras por impulso; reflita sobre a necessidade real do item antes de adquiri-lo;</li>
+                                            <li>Reveja regularmente seus gastos e ajuste seu orçamento conforme necessário para atingir suas metas;</li>
 
-                        
-                        <ul className='fs-4'>
-                            <li>Planeje seu orçamento mensal para garantir que suas despesas não ultrapassem sua renda;</li>
-                            <li>Estabeleça metas financeiras realistas e crie um plano para alcançá-las gradualmente;</li>
-                            <li>Mantenha um registro detalhado de todas as suas despesas para identificar áreas de oportunidade de economia;</li>
-                            <li>Priorize o pagamento de dívidas de juros elevados para reduzir custos a longo prazo;</li>
-                            <li>Crie uma reserva de emergência equivalente a pelo menos três meses de despesas para lidar com imprevistos;</li>
-                            <li>Considere investir parte de sua renda para construir um futuro financeiramente seguro;</li>
-                            <li>Evite compras por impulso; reflita sobre a necessidade real do item antes de adquiri-lo;</li>
-                            <li>Reveja regularmente seus gastos e ajuste seu orçamento conforme necessário para atingir suas metas;</li>
-                            <li>Negocie tarifas e taxas para garantir que está obtendo o melhor valor em serviços;</li>
-                            <li>Estabeleça limites claros para gastos em categorias específicas e adira a eles para manter o controle financeiro;</li>
-                            <li>Esteja ciente das promoções e ofertas, mas não se deixe levar por elas se não estiverem alinhadas com suas prioridades financeiras;</li>
-                            <li>Desenvolva o hábito de pesquisar preços antes de fazer grandes compras para garantir que está obtendo o melhor negócio;</li>
-                            <li>Evite acumular dívidas desnecessárias; apenas tome empréstimos ou financiamentos quando absolutamente necessário;</li>
-                            <li>Incentive a comunicação aberta sobre finanças em sua família para garantir que todos estejam alinhados com os objetivos financeiros comuns.</li>
-                        </ul>
+                                        </ul>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
 
-                    </article>
+                        <Col lg={6} md={6} className="mb-4">
+                            <Card>
+                                <Card.Body>                                    
+                                    <Card.Text>
+                                    <ul> < Dica size={150} className='fs-4' />
+                                            <li>Negocie tarifas e taxas para garantir que está obtendo o melhor valor em serviços;</li>
+                                            <li>Estabeleça limites claros para gastos em categorias específicas e adira a eles para manter o controle financeiro;</li>
+                                            <li>Esteja ciente das promoções e ofertas, mas não se deixe levar por elas se não estiverem alinhadas com suas prioridades financeiras;</li>
+                                            <li>Desenvolva o hábito de pesquisar preços antes de fazer grandes compras para garantir que está obtendo o melhor negócio;</li>
+                                            <li>Evite acumular dívidas desnecessárias; apenas tome empréstimos ou financiamentos quando absolutamente necessário;</li>
+                                            <li>Incentive a comunicação aberta sobre finanças em sua família para garantir que todos estejam alinhados com os objetivos financeiros comuns.</li>
+
+                                        </ul>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+
+
+
 
 
                 </div>
--- frontend/src/components/pages/Educacao.jsx
+++ frontend/src/components/pages/Educacao.jsx
@@ -3,16 +3,21 @@ import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
 import { Container, Row, Col, Card, } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { GiTakeMyMoney as Money } from "react-icons/gi";
 const Educacao = () => {
 
 
     const card = {
-        minHeight: "20rem"
+        minHeight: "35rem",
+        maxHeight: "35rem",
+        borderWidth: '3px',
+        overflow: 'auto'
+
     }
 
     const cardBody = {
         minHeight: "28rem"
-    }   
+    }
 
     const colorBody = {
         backgroundImage: "linear-gradient(180deg, #fff, var(--cw-primary) )"
@@ -29,12 +34,12 @@ const Educacao = () => {
 
                 <Container className="mt-5">
                     <Row>
-                                             
+
                         <Col lg={6} md={6} className="mb-4">
-                            <Card style={card}>                               
+                            <Card border="#213740" style={card} >
                                 <Card.Body className={cardBody}>
-                                <Card.Title  className='text-center'>O QUE É EDUCAÇÃO FINANCEIRA?</Card.Title>
-                                <Card.Img id='card-img' src="img/ed.png" alt="Imagem do artigo educação financeira" />                                   
+                                    <Card.Title className='text-center'>O QUE É EDUCAÇÃO FINANCEIRA?</Card.Title>
+                                    <Card.Img id='card-img' src="img/ed.png" alt="Imagem do artigo educação financeira" />
                                     <Card.Text style={{ textAlign: "justify" }}>A educação financeira engloba o conjunto de conhecimentos e habilidades fundamentais para administrar as finanças de forma abrangente. Vai muito além do simples controle dos gastos diários, envolvendo também a capacidade de planejar, criar orçamentos realistas, cultivar hábitos de economia, poupança e até mesmo explorar estratégias de investimento. Ela capacita as pessoas a gerenciarem seus recursos de maneira mais eficiente, promovendo escolhas conscientes e equilibradas em relação ao dinheiro, o que é essencial para alcançar metas financeiras e manter uma estabilidade econômica a longo prazo.
                                     </Card.Text>
                                 </Card.Body>
@@ -42,12 +47,12 @@ const Educacao = () => {
                         </Col>
 
                         <Col lg={6} md={6} className="mb-4">
-                            <Card style={card}>
-                            <Card.Body className={cardBody}>
-                            <Card.Title  className='text-center' >IMPORTÂNCIA DA EDUCAÇÃO FINANCEIRA</Card.Title>
-                                <Card.Img id='card-img' src="/img/educ.png" alt="Imagem do artigo Educação Financeira" />
-                               
-                                    
+                            <Card border="#213740" style={card}>
+                                <Card.Body className={cardBody}>
+                                    <Card.Title className='text-center' >IMPORTÂNCIA DA EDUCAÇÃO FINANCEIRA</Card.Title>
+                                    <Card.Img id='card-img' src="/img/educ.png" alt="Imagem do artigo Educação Financeira" />
+
+
                                     <Card.Text style={{ textAlign: "justify" }}>Quando a população tem um nível mais avançado de educação financeira, os benefícios se estendem para além do indivíduo. Esse conhecimento robusto não só impacta a vida pessoal, mas também influencia positivamente empresas, líderes governamentais e a sociedade como um todo. A educação financeira atua como um catalisador, capacitando as pessoas a tomarem decisões mais informadas e conscientes não apenas em questões monetárias, mas também em outras esferas da vida. Ao promover a autonomia e a habilidade de planejar de forma estratégica, ela contribui para a estabilidade econômica geral, impulsionando o crescimento sustentável das empresas, o desenvolvimento de políticas mais eficazes e uma sociedade mais resiliente e preparada para lidar com desafios diversos.
                                     </Card.Text>
                                 </Card.Body>
@@ -55,22 +60,24 @@ const Educacao = () => {
                         </Col>
 
                         <Col md={12} className="mb-4">
-                            <Card style={cardBody}>
-                            <Card.Body className={cardBody}>
-                                <Card.Img id='card-img' src="/img/previdencia.png" alt="Imagem do artigo investimento para todos" />
-                               
-                                    <Card.Title  className='text-center'>A EDUCAÇÃO FINANCEIRA PERMITE:</Card.Title>
-                                    <Card.Text style={{ textAlign: "justify" }}>
+                            <Card border="#213740" style={card}>
+                                <Card.Body className={cardBody}>
+                                    <Card.Img id='card-img' src="/img/previdencia.png" alt="Imagem do artigo investimento para todos" />
+
+                                    <Card.Title className='text-center'>A EDUCAÇÃO FINANCEIRA PERMITE:</Card.Title>
+                                    <Card.Text className="fs-5 p-3" style={{ textAlign: "justify" }}>
                                         <ul>
-                                            <li>Permite que as pessoas tenham mais controle sobre suas finanças;</li>
-                                            <li>Ajuda a tomar decisões mais conscientes e inteligentes em relação aos gastos e investimentos;</li>
-                                            <li>Evita o superendividamento precoce;</li>
-                                            <li>Retira o estigma social de que quem economiza é mesquinho e que, no fim da vida, apesar de ter dinheiro, será infeliz;</li>
-                                            <li>Retira o peso da falta de conhecimento na hora de consumir ou poupar;</li>
-                                            <li>Ajuda a desenvolver habilidades técnicas e cultura financeira;</li>
-                                            <li>Facilita a entrada dos jovens no mundo do trabalho;</li>
-                                            <li>Canal significativo para alcançar uma sociedade mais igualitária e inclusiva;</li>
-                                            <li>Ajuda a empresas a manterem e crescerem seus negócios.</li>
+                                            <p> < Money size={20} className='text-primary' />Reduz o Estresse Financeiro através do conhecimento adequado se reduz a ansiedade associada a questões monetárias, permitindo lidar melhor com imprevistos e crises econômicas pessoais;</p>
+                                            <p> < Money size={20} className='text-primary' />Estimula o Empreendedorismo que capacita indivíduos a identificar oportunidades de negócio, entender os riscos financeiros e tomar decisões mais sólidas ao iniciar empreendimentos próprios;</p>
+                                            <p> < Money size={20} className='text-primary' />Evita o superendividamento precoce;</p>
+                                            <p> < Money size={20} className='text-primary' />Favorece a Saúde Mental, pois com uma base financeira sólida, as pessoas conseguem focar mais na qualidade de vida, reduzindo problemas relacionados à saúde mental causados por problemas financeiros;</p>
+                                            <p> < Money size={20} className='text-primary' />Promove a Independência Financeira, capacitando indivíduos a serem independentes financeiramente, não dependendo apenas de fontes externas de renda, o que proporciona liberdade e autonomia;</p>
+                                            <p> < Money size={20} className='text-primary' />Estimula a Poupança para Aposentadoria por meio da Educação financeira que incentiva o planejamento a longo prazo, garantindo uma aposentadoria mais tranquila e segura;</p>
+                                            <p> < Money size={20} className='text-primary' />Contribui para a Sustentabilidade ao entender como o dinheiro impacta o mundo permite escolhas financeiras mais conscientes, como investimentos em empresas e projetos sustentáveis;</p>
+                                            <p> < Money size={20} className='text-primary' />Canal significativo para alcançar uma sociedade mais igualitária e inclusiva;</p>
+                                            <p> < Money size={20} className='text-primary' />Aumenta a Eficiência no Uso de Recursos, pois ensina a otimizar recursos disponíveis, evitando desperdícios e contribuindo para uma economia mais sustentável;</p>
+                                            <p> < Money size={20} className='text-primary' />Melhora Relações Interpessoais com o entendimento mútuo sobre finanças em relacionamentos ajuda a evitar conflitos e estabelecer metas financeiras comuns;</p>
+                                            <p> < Money size={20} className='text-primary' />Amplia Oportunidades de Investimento Social ao aprender sobre instrumentos financeiros, permitindo direcionar investimentos para causas sociais, contribuindo para o bem-estar coletivo.</p>
                                         </ul>
                                     </Card.Text>
                                 </Card.Body>
