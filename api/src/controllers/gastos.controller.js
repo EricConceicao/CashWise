47b98ff7e090fbc83c17a7ef64276b304052c52c
Renato Venícius <renato.venicius@aluno.ifsp.edu.br>
2023-12-04 18:01:43 -0300

    Renato_53

--- api/src/controllers/gastos.controller.js
+++ api/src/controllers/gastos.controller.js
@@ -64,13 +64,28 @@ export async function gastosPorCategoria(req, res) {
       },
     });
 
+    const todosGastosPorCategoria = await prisma.gasto.findMany({
+      where: {
+        data: {
+          startsWith: dataAtual,
+        }
+      },
+      select: {
+        categoria: true,
+        valor: true,
+        // Adicione outros campos do gasto que você deseja retornar
+      },
+    });
+
     const resultado = categorias.map((categoria) => {
       const categoriaAtual = categoria.categoria;
       const somatorio = somatorioPorCategoria.find((item) => item.categoria === categoriaAtual);
+      const gastosCategoria = todosGastosPorCategoria.filter((gasto) => gasto.categoria === categoriaAtual);
 
       return {
         categoria: categoriaAtual,
         totalGasto: somatorio ? somatorio._sum.valor : 0,
+        gastos: gastosCategoria,
       };
     });
 
