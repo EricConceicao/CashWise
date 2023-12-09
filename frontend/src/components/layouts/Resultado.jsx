3d7a8b6d9ce2c6b121ed4814cf544dc30adc8117
Eric <eric.r@aluno.ifsp.edu.br>
2023-12-09 20:37:52 -0300

    Finalmente: Arrumado com uma (conditional rendering) o header se alternar caso o usuário esteja logado, ou não. Além de ser possível voltar à home, ou à landing clicando no ícone.

--- frontend/src/components/layouts/Resultado.jsx
+++ frontend/src/components/layouts/Resultado.jsx
@@ -12,8 +12,6 @@ function ResultadoPage() {
 	const location = useLocation();
 	const simulationResults = location.state.result;
 
-	console.log(simulationResults);
-
 	const generoLabel = simulationResults.simulacao.genero === 'm' ? 'Masculino' : 'Feminino';
 	const navigate = useNavigate();
 
@@ -24,75 +22,62 @@ function ResultadoPage() {
 			<header className='bg-info text-center'>
 				<h1 className='display-1'>Resultados da sua simulação</h1>
 			</header>
-			<main className="main">
-				<Container fluid>
-					<section>
-						<header>
-							<h2>Dados Pessoais</h2>
-						</header>
-						<p>
-							<i>Gênero:</i> <span>{generoLabel}</span>
-						</p>
-						<p>
-							<i>Idade Atual:</i> <span>{simulationResults.idadeAtual} anos</span>
-						</p>
-						<p>
-							<i>Tempo de Contribuição: </i>
-							<span>{simulationResults.simulacao.tempo_contribuicao_mes} meses</span>
-						</p>
-					</section>
 
-					<div className="titulo">
-						<h2>
-							<b>Resultados</b>
-						</h2>
-					</div>
-					<div className="barra">
-						<p>
-							<i>Idade da Aposentadoria:</i>{' '}
-							<span>{simulationResults.idadeAposentadoria} anos</span>
-						</p>
-						<p>
-							<i>Mês e ano da Aposentadoria:</i>{' '}
-							<span>
-								{simulationResults.mesAposentadoria} / {simulationResults.anoAposentadoria}
-							</span>
-						</p>
-						<p>
-							<i>Quanto tempo falta para contribuir:</i>
-							<br />
-							<span>{simulationResults.tempoContribuicaoPendente} meses</span>
-						</p>
-						<p>
-							<i>O valor mínimo da minha aposentadoria será:</i>{' '}
-							<span id="valorAposentadoria">
-								R$ {parseFloat(simulationResults.valorAposentadoria).toLocaleString('pt-BR', { maximumFractionDigits: 2 })}
-							</span>
-						</p>
+			<main className="container-fluid my-4">
+				<Row className='align-items-center bg-light m-1 rounded-2 border border-2'>
+					<Col className='d-flex justify-content-center' xm={12} sm={6} md={6}>
+						<img src="img/senhor.png" alt="Senhor apresentando os resultados à sua direita" width="75%" />
+					</Col>
 
-						<p>
-							<i>O valor mínimo do meu auxílio doença será:</i>{' '}
-							<span id="valorAuxilioDoenca">
-								R$ {parseFloat(simulationResults.valorAuxilioDoenca).toLocaleString('pt-BR', { maximumFractionDigits: 2 })}
-							</span>
-						</p>
-					</div>
-				</Container>
+					<Col xm={12} sm={6} md={6}>
+						<section className='m-1'>
+							<header className='bg-primary px-1 rounded-2'>
+								<h2>Dados Pessoais</h2>
+							</header>
+							
+							<dl className='bg-info p-2 rounded border'>
+								<dt>Gênero</dt>
+								<dd className='bg-light rounded p-1 border'>{generoLabel}</dd>
 
-				<div className="botao">
-					<Button
-						variant="secondary"
-						className="envio"
-						onClick={() => {
-							navigate('/simule');
-						}}
-					>
-						Nova Consulta
-					</Button>
-				</div>
-				<div className="imagem">
-					<img src="img/senhor.png" alt="" width="200px" />
-				</div>
+								<dt>Idade Atual</dt>
+								<dd className='bg-light rounded p-1 border'>{simulationResults.idadeAtual} anos</dd>
+
+								<dt>Tempo de Contribuição</dt>
+								<dd className='bg-light rounded p-1 border'>{simulationResults.simulacao.tempo_contribuicao_mes} meses</dd>
+							</dl>				
+						</section>
+
+
+						<section className='m-1'>
+							<header>
+								<h2 className='bg-primary px-1 rounded-2'>Resultados</h2>
+							</header>
+
+							<dl className='bg-info p-2 rounded border'>
+								<dt>Idade da Aposentadoria</dt>
+								<dd className='bg-light rounded p-1 border'>{simulationResults.idadeAposentadoria} anos</dd>
+
+								<dt>Mês e ano da Aposentadoria</dt>
+								<dd className='bg-light rounded p-1 border'>{simulationResults.mesAposentadoria} / {simulationResults.anoAposentadoria}</dd>
+
+								<dt>Quanto tempo falta para contribuir</dt>
+								<dd className='bg-light rounded p-1 border'>{simulationResults.tempoContribuicaoPendente} meses</dd>
+
+								<dt>O valor mínimo da minha aposentadoria será</dt>
+								<dd className='bg-light rounded p-1 border'>R$ {parseFloat(simulationResults.valorAposentadoria).toLocaleString('pt-BR', { maximumFractionDigits: 2 })}</dd>
+							
+								<dt>O valor mínimo do meu auxílio doença será</dt>
+								<dd className='bg-light rounded p-1 border'>R$ {parseFloat(simulationResults.valorAuxilioDoenca).toLocaleString('pt-BR', { maximumFractionDigits: 2 })}</dd>
+							</dl>
+						</section>
+					</Col>
+				
+					<Col className="d-flex justify-content-center p-2" sm={12}>
+						<Button	size="lg" variant="secondary" onClick={() => navigate('/simule')}>
+							Nova Consulta
+						</Button>
+					</Col>
+				</Row>
 			</main>
 		</div>
 	);
